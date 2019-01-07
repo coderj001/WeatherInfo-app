a9fadff1e0eb22cc2215b218791676c16df8b386
raju ghorai <39460245+coderj001@users.noreply.github.com>
2019-01-07 11:00:48 +0530

    Most of the error have been removed.
    Previously the app was not showing any info as plugins were not added which have been added and the algorithm has been improved for the program to run efficiently. And comment has been added on index.js file for better understanding.

--- www/index.js
+++ www/index.js
@@ -1,24 +1,44 @@
-//Jascript Object Document 
-xhr=new XMLHttpRequest();
-// get an XMLHttpRequst fun to xhr
-
-url="http://api.apixu.com/v1/current.json?key= 351f428e95d64053a2c150636182312&q="; // this link with api key
-function disp(){
-
-    var entry=document.getElementById("search").value; // geting the value of search box
-    
-    xhr.open('GET',url+entry,true)
-xhr.responseType='text';
-xhr.send();
+/* 
+This javascript code build for creating cordova/phonegap application who's uses to show the current weather information for any entered location.
+*/
+function onLoad() {
+    if(navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|Netscape)/)){
+        document.addEventListener('deviceready', onDeviceReady(), false);
+    }           // onLoad funtion will be called when the body tag is loaded. Within the onLoad function i have called onDeviceReady(). The condition is for checking if it is open by devices like android, iPad...etc contain event listener to check if device is ready and else for any browser.
+    else{
+        onDeviceReady()
+    }
+    // console.log("H")
 }
-xhr.onload = function(){
-    myObj=JSON.parse(xhr.response);
-    console.log(myObj);
-    document.getElementById("city").innerHTML=myObj.location.name;
-document.getElementById("state").innerHTML=myObj.location.region;
-document.getElementById("country").innerHTML=myObj.location.country;
-document.getElementById("temp").innerHTML=myObj.current.temp_c +"&deg;";
-document.getElementById("lat").innerHTML=myObj.location.lat;
-document.getElementById("lon").innerHTML=myObj.location.lon;
-document.getElementById("hum").innerHTML=myObj.current.humidity;
+function onDeviceReady(e){      // onDeviceReady() function is called by onLoad function it use to request information about weather via JSON format.  It help to update web page with out reloading.
+    // console.log("hello1")
+
+    xhr = new XMLHttpRequest();     // XMLHttpRequest object can be used to request data from web server.
+
+    var url="http://api.apixu.com/v1/current.json?key=351f428e95d64053a2c150636182312&q=";      // url contain link of the web server which provide information as JSON token.
+
+    document.getElementById("searchButton").addEventListener('click', function () {     // this for adding click event on the button tag.
+        var sh=document.getElementById("searchBox").value;  // storing the qury on sh 
+        xhr.open('GET',url+sh,true);    // requesting via GET methord.
+        xhr.responseType='text';
+        xhr.send();
+        // console.log("hello2")
+		navigator.vibrate(1200)   // This for vibration the phone when ever the button is clicked
+        document.getElementById('container').hidden=false;
+
+        xhr.onload=function () {
+            myObj=JSON.parse(xhr.response);     // convertion string JSON to object
+            // console.log(myObj)
+            // rest for taking the information from requested JSON object and storing in HTML tags.
+            document.getElementById('showTemperatureC').innerHTML=myObj.current.temp_c + '&deg; C';
+            document.getElementById('showTemperatureF').innerHTML=myObj.current.temp_f + '&deg; F';
+            document.getElementById('showHumidity').innerHTML=myObj.current.humidity +' <small><b>(per cubic meter)</b></small>';
+            document.getElementById('icon').src="http://"+myObj.current.condition.icon;
+            document.getElementById('Text').innerText=myObj.current.condition.text;
+            document.getElementById('location').innerHTML=myObj.location.name+', &nbsp;'+myObj.location.region+', &nbsp;'+myObj.location.country;
+            document.getElementById('showLatitude').innerHTML=myObj.location.lat+'&deg;';
+            document.getElementById('showLongitude').innerHTML=myObj.location.lon+'&deg;';
+        }
+    }), false
 }
+
